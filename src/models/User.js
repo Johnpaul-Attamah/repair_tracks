d26a7f8cf36c19ec4ddd0dae1a1af60b8aa5d26d
Johnpaul <bohrplus@gmail.com>
2020-08-04 13:19:23 +0100

    create user model [#173997381] * create User collection     * create src/models/User.js     * create UserSchema using mongoose.     * export UserSchema.

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+export default mongoose.model('User', UserSchema);
