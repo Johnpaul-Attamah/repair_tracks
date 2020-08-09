67e227cb48897a78fb48c93a2f1aa7679f8da711
Johnpaul <bohrplus@gmail.com>
2020-08-09 04:07:30 +0100

    fixing code coverage

--- src/models/Connect.js
+++ src/models/Connect.js
@@ -3,16 +3,15 @@ dotenv.config();
 
 let url = '';
 
-console.log(process.env.NODE_ENV)
-
-if (process.env.NODE_ENV.trim() === 'production') {
+if (process.env.NODE_ENV === 'production') {
     url = process.env.PROD_DB_URI;
 }
-if (process.env.NODE_ENV.trim() === 'test') {
+if (process.env.NODE_ENV === 'test') {
     url = process.env.TEST_DB_URI;
 }
-if (process.env.NODE_ENV.trim() === 'development') {
+if (process.env.NODE_ENV === 'development') {
     url = process.env.DEV_DB_URI;
 }
 
+console.log(url)
 export default url;
