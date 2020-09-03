8d26caa6bbffcfa6b19ff6dcf4c03b0d078f9246
Johnpaul <bohrplus@gmail.com>
2020-09-03 16:33:41 +0100

    [#174646338] Test to create supervisor - create test/admin.js - write test to get supervisor id. - write test to login a test user - write test to login the admin - write test that fail if no token - write test that fails if test user tries to create supervisor - write test that gives 404 if admin tries to create supervisor with unknown id. - write test that creates a supervisor.

---
+++ src/test/testadmin.js
@@ -0,0 +1,109 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server';
+import User from '../models/User';
+
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Administrator', () => {
+
+    const fakeToken = '56hhhi88090990-09jjhbbbtggbll*nbkj';
+    const testUserToken = {};
+    const adminUserToken = {};
+    const testUserId = {id: '5f50ee81ec72c9148cafe77d'};
+    const superVisorId = {};
+
+    before((done) => {
+        User.findOne({
+                    email: 'supervisor@email.com'
+                }, (err, res) => {
+                    superVisorId.id = res._id;
+            done();
+        })
+    });
+
+    before((done) => {
+            const user = {
+                inputValue: 'scapegoat@email.com',
+                password: 'scapeGOAT2020',
+            };
+            chai.request(server)
+                .post('/api/v1/auth/login')
+                .send(user)
+                .end((err, res) => {
+                    testUserToken.token = res.body.token;
+                    done();
+                });
+        });
+    before((done) => {
+            const user = {
+                inputValue: 'administrator@email.com',
+                password: 'superADMIN001',
+            };
+            chai.request(server)
+                .post('/api/v1/auth/login')
+                .send(user)
+                .end((err, res) => {
+                    adminUserToken.token = res.body.token;
+                    done();
+                });
+        });
+
+    
+    describe('/put - create admin user', () => {
+        it("Should not create supervisor when token didn't match", (done) => {
+            chai.request(server)
+                .put(`/api/v1/user/supervisor/${superVisorId.id}`)
+                .set('Authorization', fakeToken)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.body.should.be.eql({});
+                    done(err);
+                });
+        });
+        it("Should not create supervisor when user is not admin ", (done) => {
+            chai.request(server)
+                .put(`/api/v1/user/supervisor/${superVisorId.id}`)
+                .set('Authorization', testUserToken.token)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    res.body.errors.should.have.property('noPermission').eql('Only administrators can create a supervisor');
+                    res.body.should.have.property('status').eql('failed');
+                    done();
+                });
+        });
+        it("Should not create supervisor when user id is not found", (done) => {
+            chai.request(server)
+                .put(`/api/v1/user/supervisor/${testUserId.id}`)
+                .set('Authorization', adminUserToken.token)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    res.body.errors.should.have.property('userNotFound').eql('user not found');
+                    res.body.should.have.property('status').eql('success');
+                    done();
+                });
+        });
+        it("Should create supervisor.", (done) => {
+            chai.request(server)
+                .put(`/api/v1/user/supervisor/${superVisorId.id}`)
+                .set('Authorization', adminUserToken.token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('supervisor');
+                    res.body.supervisor.should.have.property('role').eql('supervisor');
+                    res.body.should.have.property('message').eql('user updated successfully');
+                    res.body.should.have.property('status').eql('success');
+                    done();
+                });
+        });
+    });
+    
+});
