40b26461ba63059522a124543461c02f75f18df2
Johnpaul <bohrplus@gmail.com>
2020-09-03 14:28:08 +0100

    [#174002446] Create supervisor - create new controller admin.js - create routes - validate all possible cases. - create supervisor only when user is admin - test endpoint on postman.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 import userControllers from '../controllers/user';
 import profileControllers from '../controllers/profile';
 import requestControllers from '../controllers/request';
+import adminControllers from '../controllers/admin';
+
 
 const router = app => {
     app.use('/api/v1/auth', userControllers);
     app.use('/api/v1/profile', profileControllers);
+    app.use('/api/v1/user', adminControllers);
     app.use('/api/v1/request', requestControllers);
     app.use((req, res, next) =>{
         res.status(200).json({ greetings: 'Welcome to repairs_tracks' });
