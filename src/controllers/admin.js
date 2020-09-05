48f8939e5b830a0d99febe683296c5c468d83bde
Johnpaul <bohrplus@gmail.com>
2020-09-05 02:05:14 +0100

    [#174665037] Get all supervisors - write controller action. - get user from db with supervisor role - fet supervisors without including passport details and unneccessary stuff

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -86,4 +86,48 @@ const router = express.Router();
         }
       });
 
+
+/**
+ * @route PUT api/v1/user/engineer
+ * @access Private Access 
+ * @description create engineer
+ ***/
+
+ router.get('/supervisor/all', passport.authenticate('jwt', {
+      session: false }), async (req, res) => {
+        const errors = {};
+
+        try {
+            if (req.user.role === 'admin') {
+              const supervisors = await User.find({ role: 'supervisor'})
+                if (!supervisors) {
+                  errors.noSupervisors = 'There are no supervisors present';
+                  return res.status(404).json({
+                    status: 'success',
+                    errors
+                  });
+                }
+                const outputValue = supervisors.map(value =>( {
+                  username: value.username,
+                  name: value.name,
+                  email: value.email,
+                  avatar:value.avatar,
+                  role: value.role
+                }))
+              return res.status(200).json({
+                status: 'success',
+                message: 'supervisors fetched successfully',
+                outputValue
+              })
+          }
+            errors.noPermission = 'Only administrators can view supervisors';
+            return res.status(401).json({
+              status: 'failed',
+              errors
+            });
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+      });
+
 export default router;
